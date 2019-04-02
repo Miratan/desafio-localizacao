3fa5f6006217a4d668d48e1aa033c7e561b07708
Miratan Lehmkuhl <miratansc@gmail.com>
2019-04-02 02:05:09 -0300

    Initial structure - backend

---
+++ desafio-localizacao-server/src/main/java/com/desafiolocalizacaoserver/DesafioLocalizacaoServerApplication.java
@@ -0,0 +1,13 @@
+package com.desafiolocalizacaoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLocalizacaoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLocalizacaoServerApplication.class, args);
+	}
+
+}
